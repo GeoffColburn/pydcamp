6a02b980a0fda99bf3de7bd84d31ddf1d1194e5c
Geoffrey Colburn <GeoffreyColburn@gmail.com>
2012-05-04 12:10:27 -0500

    samtools faidx command applied to reference in Breakdancer pipeline.

--- src/dcamp.py
+++ src/dcamp.py
@@ -91,6 +91,7 @@ def do_breakdancer(args):
     """Believe we need to keep all files in one directory, although it's not mentioned,
     breakdancer may look at files other than the .bam file."""
     fasta_path = pipelines.common.prepare_reference(args, "01_All")
+    pipelines.samtools.faidx(fasta_path)
     sorted_bam_path = pipelines.common.bwa_alignment(args, fasta_path, "01_All")
 
     sorted_bam_file = os.path.basename(sorted_bam_path)
